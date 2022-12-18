38433f2179e5c2334a085084c502e6a69d5714a9
thedonik <doniyoreshkulov2504@gmail.com>
2022-12-18 23:19:20 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+var year = prompt("Yil", "2022");
+var month = prompt("oy", "dekabr");
+var firstName = prompt("Ismingizni kiriting");
+var lastName = prompt("Familiyangizni kiriting");
+var coutry = prompt("Davlat");
+var city = prompt("Shahar");
+var age = prompt("Yoshingiz");
+var song = prompt("Qushq");
+var movie = prompt("Kino");
+var high = prompt("buyingiz");
+
+var fullList = 
+"Yil: "  + year + "\n" +
+"Oy: " + month + "\n" + 
+"Ism: " + firstName + "\n" +
+"Familiya: " + lastName + "\n" +
+"Davlat: " + coutry + "\n" +
+"Shahar: " + city + "\n" +
+"Yosh: " + age + "\n" +
+"Qushiq: " + song + "\n" +
+"Buyingiz: " + high + "\n" +
+
+
+
+
+alert(fullList);
+console.log(fullList);
+
+
+
